259aa1d69403d50e6e4f4d2bcc976065d294b196
Oleksandr Kondratiuk <48170652+alex-kondr@users.noreply.github.com>
2024-11-22 14:52:34 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ import requests
 response = requests.get("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=11")
 exchange_json = response.json()
 
-usd_exchenge = [exchange_dict for exchange_dict in exchange_json if exchange_dict.get("ccy") == "USD"][0]
+usd_exchange = [exchange_dict for exchange_dict in exchange_json if exchange_dict.get("ccy") == "USD"][0]
 
 print(usd_exchange.get("buy"))
